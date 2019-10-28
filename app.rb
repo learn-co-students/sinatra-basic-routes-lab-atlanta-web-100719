cf6ca2f6af3f20dbb31aa7df9b50ff18b6285ada
clarkj99 <clark@stontech.net>
2019-10-28 11:37:21 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
-require_relative 'config/environment'
+require_relative "config/environment"
 
 class App < Sinatra::Base
+  get "/" do
+    "What's crackin?"
+  end
+
+  get "/name" do
+    "My name is Clark"
+  end
+
+  get "/hometown" do
+    "My hometown is Augusta"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is Oldtown Road"
+  end
 end
