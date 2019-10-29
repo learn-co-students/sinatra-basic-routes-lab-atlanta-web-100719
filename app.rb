a2d6e07630adde2e09f7a6d7a0a8cd74eab2fe19
Kuran <kurankumar@gmail.com>
2019-10-29 13:28:36 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
-require_relative 'config/environment'
+require_relative "config/environment"
 
 class App < Sinatra::Base
+  get "/" do
+    "Hello, World!"
+  end
+
+  get "/name" do
+    "My name is Kuran"
+  end
+
+  get "/hometown" do
+    "My hometown is Kansas City, Missouri"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is Jump by Mac Miller"
+  end
 end
